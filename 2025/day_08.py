e8c0fdcf5e51c6a351b6e9687fb57f8246018f0a
Dave Gwilt <davegwilt@gmail.com>
2025-12-08 08:39:02 +0000

    Update day_08.py

--- 2025/day_08.py
+++ 2025/day_08.py
@@ -6,8 +6,7 @@ from math import prod,sqrt
 
 class AocDay8(AocDay):
 
-    def dist(a,b):
-        return sqrt((a[0]-b[0])**2 + (a[1]-b[1])**2 + (a[2]-b[2])**2)
+    dist = lambda a,b : sqrt((a[0]-b[0])**2 + (a[1]-b[1])**2 + (a[2]-b[2])**2)
     
     def distances(data):
         boxes = [tuple(int(i) for i in line.split(",")) for line in data.splitlines()]
